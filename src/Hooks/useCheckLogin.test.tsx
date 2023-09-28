64780f331e5fbbbcb265caa936551ff5504f3d6a
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-09-28 18:30:30 +0700

    unit test custom hook still failed

--- src/Hooks/useCheckLogin.test.tsx
+++ src/Hooks/useCheckLogin.test.tsx
@@ -1,14 +1,30 @@
-import { renderHook, act} from "@testing-library/react";
-import useCheckLogin from "./UseCheckLogin";
-import result from "antd/es/result";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useCheckLogin } from '.'; // Adjust the import path as needed
 
-describe('Hooks useCheckLogin test', () => {
-    test('test increment fuction' () =>{
-        const { result} = renderHook(()=> useCheckLogin()),
-        act(() => {
-            result.current.increment();
-        });
+// Mock the react-router-dom's useNavigate hook
+jest.mock('react-router-dom', () => ({
+  useNavigate: jest.fn(),
+}));
 
-        expect(result.current.count).toBe(1)
-    })
-})
+describe('useCheckLogin', () => {
+  it('should navigate to the home page when token is null', () => {
+    const navigateMock = jest.fn();
+    require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+
+    const { unmount } = render(<ComponentWithCheckLogin token={null} />); // Replace ComponentWithCheckLogin with your actual component
+
+    expect(navigateMock).toHaveBeenCalledWith('/');
+    unmount();
+  });
+
+  it('should not navigate when token is not null', () => {
+    const navigateMock = jest.fn();
+    require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+
+    const { unmount } = render(<ComponentWithCheckLogin token="some-token" />); // Replace ComponentWithCheckLogin with your actual component
+
+    expect(navigateMock).not.toHaveBeenCalled();
+    unmount();
+  });
+});
