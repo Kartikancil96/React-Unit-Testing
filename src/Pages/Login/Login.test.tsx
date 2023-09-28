81f89e923152b180a35082286263a91813089513
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-09-28 17:57:27 +0700

    masih fail login tst

--- src/Pages/Login/Login.test.tsx
+++ src/Pages/Login/Login.test.tsx
@@ -1,50 +1,70 @@
-import { waitFor, fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import Login from ".";
 
-describe('test login form', () => {
-    const mockProps = jest.fn();
-    test('title render correctly', async () => {
-        render(<Login onSubmit={mockProps} />)
-        const title = screen.getByText('Login')
-        expect(title).toBeDefined();
-    })
+// Mock the useNavigate function from react-router-dom
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => jest.fn(),
+}));
 
-    test('label email render correctly', async () => {
-        render(<Login onSubmit={mockProps} />)
-        const title = screen.getByText('Email Address')
-        expect(title).toBeDefined();
-    })
+// Mock the fetch function
+global.fetch = jest.fn().mockResolvedValue({
+    json: () => Promise.resolve({ data: { token: "mock-token" } }),
+  });
 
-    test('label password render correctly', async () => {
-        render(<Login onSubmit={mockProps} />);
-        const title = screen.getByText('Password')
-        expect(title).toBeDefined();
-    })
+describe("Login Component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    test('button submit render correctly', async () => {
-        render(<Login onSubmit={mockProps} />)
-        const title = screen.getByText('Log in')
-        expect(title).toBeDefined();
-    })
+  it("renders without errors", () => {
+    const { getByText, getByPlaceholderText } = render(<Login />);
+    
+    // Check if the component renders the "Login" header text
+    expect(getByText("Login")).toBeDefined();
+    
+    // Check if email and password input fields are present
+    expect(getByPlaceholderText("ex@ample.com")).toBeDefined();
+    expect(getByPlaceholderText("Password")).toBeDefined();
+    
+    // Check if the "Log in" button is present
+    expect(getByText("Log in")).toBeDefined();
+  });
 
-    test('onSubmit work correctly', async () => {
-        const { getByPlaceholderText, getByText } = render(<Login onSubmit={mockProps} />);
-        const emailInput = getByPlaceholderText('ex@ample.com') as HTMLInputElement;
-        const passwordInput = getByPlaceholderText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log in');
+  it("handles form submission and redirects on successful login", async () => {
+    const { getByPlaceholderText, getByText } = render(<Login />);
+    const emailInput = getByPlaceholderText("ex@ample.com");
+    const passwordInput = getByPlaceholderText("Password");
+    const loginButton = getByText("Log in");
 
-        fireEvent.change(emailInput, {target: { value: 'siapa@dimana.com' }});
-        fireEvent.change(passwordInput, { target: { value: '12345678' } });
+    // Simulate user input
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
 
-        fireEvent.click(submitButton);
+    // Simulate form submission
+    fireEvent.click(loginButton);
 
-        await waitFor(() => {
-            expect(mockProps).toHaveBeenCalledTimes(1);
-            expect(mockProps).toHaveBeenCalledWith({
-                email: 'siapa@dimana.com',
-                password: '12345678',
-            });
-        });
+    // Wait for fetch to be called
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledWith(
+        "https://mock-api.arikmpt.com/api/user/login",
+        expect.objectContaining({
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: "test@example.com",
+            password: "password123",
+          }),
+        })
+      );
     });
 
-})
+    // Check if the user is redirected to "/home"
+    // This assumes that your useNavigate mock is set up to return a function
+    // that can be used to check if it was called with the correct path
+    const navigateMock = require("react-router-dom").useNavigate();
+    expect(navigateMock).toHaveBeenCalledWith("/home");
+    
+    // You can also check if the token was stored in localStorage if needed
+  });
+});
--- src/Pages/Register/Register.test.tsx
+++ src/Pages/Register/Register.test.tsx
@@ -1,71 +1,74 @@
-import { waitFor, fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import Register from ".";
 
-describe('test register form', () => {
-    const mockProps = jest.fn();
-    test('title render correctly', async () => {
-        render(<Register onSubmit={mockProps} />)
-        const title = screen.getByText('Register Account')
-        expect(title).toBeDefined();
-    })
+describe("Register Component", () => {
+  it("renders the component correctly", () => {
+    const { getByText, getByPlaceholderText } = render(<Register />);
 
-    test('label name render correctly', async () => {
-        render(<Register onSubmit={mockProps} />)
-        const title = screen.getByText('Name')
-        expect(title).toBeDefined();
-    })
+    // Assert that the component's title and form elements are rendered
+    expect(getByText("Register Account")).toBeDefined();
+    expect(getByPlaceholderText("Your Name")).toBeDefined();
+    expect(getByPlaceholderText("ex@ample.com")).toBeDefined();
+    expect(getByPlaceholderText("Password")).toBeDefined();
+  });
 
-    test('label email render correctly', async () => {
-        render(<Register onSubmit={mockProps} />)
-        const title = screen.getByText('Email Address')
-        expect(title).toBeDefined();
-    })
+  it("validates form submission", async () => {
+    const { getByText, getByPlaceholderText } = render(<Register />);
 
-    test('label password render correctly', async () => {
-        render(<Register onSubmit={mockProps} />);
-        const title = screen.getByText('Password')
-        expect(title).toBeDefined();
-    })
+    // Submit the form without filling in any fields
+    fireEvent.click(getByText("Sign Up"));
 
-    test('button submit render correctly', async () => {
-        render(<Register onSubmit={mockProps} />)
-        const title = screen.getByText('Sign Up')
-        expect(title).toBeDefined();
-    })
-    test('button submit render correctly', async () => {
-        render(<Register onSubmit={mockProps} />)
-        const title = screen.getByText('Sign Up')
-        expect(title).toBeDefined();
-    })
+    // Assert that validation error messages are displayed
+    await waitFor(() => {
+      expect(getByText("Please input your Full Name")).toBeDefined();
+      expect(getByText("Email is Required")).toBeDefined();
+      expect(getByText("Password is required")).toBeDefined();
+    });
+  });
 
-    test('button submit render correctly', async () => {
-        render(<Register onSubmit={mockProps} />)
-        const title = screen.getByText('Back')
-        expect(title).toBeDefined();
-    })
+  it("handles form submission and redirects on success", async () => {
+    // Mock the fetch function to simulate a successful registration
+    global.fetch = jest.fn().mockResolvedValue({
+        json: () => Promise.resolve({ data: { token: "mock-token" } }),
+      });
 
-    test('onSubmit work correctly', async () => {
-        const { getByPlaceholderText, getByText } = render(<Register onSubmit={mockProps} />);
-        const nameInput = getByPlaceholderText('Your Name') as HTMLInputElement;
-        const emailInput = getByPlaceholderText('ex@ample.com') as HTMLInputElement;
-        const passwordInput = getByPlaceholderText('Password') as HTMLInputElement;
-        const submitButton = getByText('Sign Up');
-        const backButton = getByText('Back');
+    const { getByText, getByPlaceholderText, history } = render(<Register />);
 
-        fireEvent.change(nameInput, {target: { value: 'test' }});
-        fireEvent.change(emailInput, {target: { value: 'siapa@dimana.com' }});
-        fireEvent.change(passwordInput, { target: { value: '12345678' } });
-
-        fireEvent.click(submitButton, backButton);
-
-        await waitFor(() => {
-            expect(mockProps).toHaveBeenCalledTimes(1);
-            expect(mockProps).toHaveBeenCalledWith({
-                name: 'test',
-                email: 'siapa@dimana.com',
-                password: '12345678',
-            });
-        });
+    // Fill in the form fields
+    fireEvent.change(getByPlaceholderText("Your Name"), {
+      target: { value: "John Doe" },
+    });
+    fireEvent.change(getByPlaceholderText("ex@ample.com"), {
+      target: { value: "john@example.com" },
+    });
+    fireEvent.change(getByPlaceholderText("Password"), {
+      target: { value: "password123" },
     });
 
-})
+    // Submit the form
+    fireEvent.click(getByText("Sign Up"));
+
+    // Assert that fetch was called with the correct data
+    expect(fetch).toHaveBeenCalledWith(
+      "https://mock-api.arikmpt.com/api/user/register",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: "John Doe",
+          email: "john@example.com",
+          password: "password123",
+        }),
+      }
+    );
+
+    // Assert that localStorage is updated
+    expect(localStorage.setItem).toHaveBeenCalledWith("token", "mockToken");
+
+    // Assert that it redirects after successful registration
+    await waitFor(() => {
+      expect(history.location.pathname).toBe("/");
+    });
+  });
+});
