b414c46e957959a4d47c067014e8524b45062cf8
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-09-30 20:30:53 +0700

    fix ts config

--- src/__mocks__/react-router-dom.ts
+++
@@ -1,9 +0,0 @@
-// src/__mocks__/react-router-dom.ts
-
-import * as ReactRouterDom from 'react-router-dom';
-
-const originalModule = jest.requireActual('react-router-dom');
-
-export const useNavigate = jest.fn();
-
-export { originalModule as default, useNavigate };
