cd3d7058203aacf851c096ef34df19a8fa97d85a
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-09-30 19:07:44 +0700

    unit test add dan edit still fail

--- src/Pages/Edittable/Edittable.test.tsx
+++ src/Pages/Edittable/Edittable.test.tsx
@@ -1,62 +1,103 @@
-import React from "react";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
-import Edittable from "."; // Update the import path as per your project structure
+import { render, fireEvent, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Edittable from ".";
 
-// Mock the react-router-dom useParams hook
-jest.mock("react-router-dom", () => ({
-  useParams: jest.fn().mockReturnValue({ id: "1" }), // Replace with the appropriate ID for testing
-}));
-
-describe("Edittable Component", () => {
-  it("renders without errors", () => {
-    render(<Edittable />);
-    expect(screen.getByText("Edit Data Form")).toBeDefined();
+describe("Edittable Component Unit Tests", () => {
+  test("renders the title", () => {
+    render(
+      <MemoryRouter>
+        <Edittable />
+      </MemoryRouter>
+    );
+    const titleElement = screen.getByText("Edit Data Form");
+    expect(titleElement).toBeDefined();
   });
 
-  it("handles form submission", async () => {
-    // Mock the fetch function to simulate API call
-    global.fetch = jest.fn().mockResolvedValue({
-        json: () => Promise.resolve({ data: { token: "mock-token" } }),
-      });
-
-    render(<Edittable />);
-    
-    // Fill in the form fields
-    fireEvent.change(screen.getByPlaceholderText("Your Name"), {
-      target: { value: "John Doe" },
-    });
-    
-    fireEvent.click(screen.getByText("Active")); // Choose an option from the dropdown
-
-    fireEvent.click(screen.getByText("Edit Data"));
-
-    // Wait for the fetch call to resolve
-    await waitFor(() => {
-      expect(global.fetch).toHaveBeenCalledWith(
-        "https://mock-api.arikmpt.com/api/category/update",
-        expect.objectContaining({
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: expect.stringContaining("Bearer"),
-          },
-          body: JSON.stringify({
-            id: "1", // Mocked useParams value
-            name: "John Doe",
-            is_active: "true", // Chosen option from dropdown
-          }),
-        })
-      );
-    });
-
-    // Check if navigation occurred after form submission
-    expect(screen.queryByText("Edit Data Form")).not.toBeDefined();
-    expect(screen.getByText("Back")).toBeDefined();
+  test("renders the card", () => {
+    render(
+      <MemoryRouter>
+        <Edittable />
+      </MemoryRouter>
+    );
+    const cardElement = screen.getByTestId("edit-card");
+    expect(cardElement).toBeDefined();
   });
 
-  it("handles navigation back to home", () => {
-    const { container } = render(<Edittable />);
-    fireEvent.click(screen.getByText("Back"));
-    // You can add assertions to check if navigation occurred correctly
+  test("renders the form", () => {
+    render(
+      <MemoryRouter>
+        <Edittable />
+      </MemoryRouter>
+    );
+    const formElement = screen.getByTestId("edit-form");
+    expect(formElement).toBeDefined();
+  });
+
+  test("renders the label form", () => {
+    render(
+      <MemoryRouter>
+        <Edittable />
+      </MemoryRouter>
+    );
+    const nameLabelElement = screen.getByLabelText("Your Name");
+    const statusLabelElement = screen.getByLabelText("Select Status");
+
+    expect(nameLabelElement).toBeDefined();
+    expect(statusLabelElement).toBeDefined();
+  });
+
+  test("renders the buttons", () => {
+    render(
+      <MemoryRouter>
+        <Edittable />
+      </MemoryRouter>
+    );
+    const editButtonElement = screen.getByText("Edit Data");
+    const backButtonElement = screen.getByText("Back");
+
+    expect(editButtonElement).toBeDefined();
+    expect(backButtonElement).toBeDefined();
+  });
+
+  test("submitting the form calls handleEdit function", () => {
+    // Mock the fetch function and localStorage
+    const mockFetch = jest.fn(() => Promise.resolve({}));
+    global.fetch = mockFetch;
+    const mockLocalStorage = {
+      getItem: jest.fn(() => "mockToken"),
+    };
+    Object.defineProperty(window, "localStorage", { value: mockLocalStorage });
+
+    render(
+      <MemoryRouter>
+        <Edittable />
+      </MemoryRouter>
+    );
+
+    // Fill out the form
+    const nameInput = screen.getByPlaceholderText("Your Name");
+    const statusSelect = screen.getByPlaceholderText("Select Status");
+    const editButton = screen.getByText("Edit Data");
+
+    fireEvent.change(nameInput, { target: { value: "John Doe" } });
+    fireEvent.change(statusSelect, { target: { value: "true" } });
+    fireEvent.click(editButton);
+
+    // Verify that handleEdit function is called with the correct values
+    expect(mockFetch).toHaveBeenCalledWith(
+      "https://mock-api.arikmpt.com/api/category/update",
+      expect.objectContaining({
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer mockToken",
+        },
+        body: JSON.stringify({
+          id: "mockId", // You should set the id value for testing
+          name: "John Doe",
+          is_active: "true",
+        }),
+      })
+    );
   });
 });
