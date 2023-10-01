ee09e53cc0d0430da7f0cb09e709a7a5a6d08f3a
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-10-01 20:58:42 +0700

    unit testing Addtable pass

--- src/Pages/Addtable/Addtable.test.tsx
+++ src/Pages/Addtable/Addtable.test.tsx
@@ -1,53 +1,46 @@
-import { render, fireEvent, waitFor } from "@testing-library/react";
-import Addtable from "."; // Import your component
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Addtable from ".";
 
-describe("Addtable Component", () => {
-  it("renders the component without errors", () => {
-    render(<Addtable />);
-  });
-
-  it("submits the form successfully", async () => {
-    // Mock the fetch function
-    const mockFetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({ success: true }),
-      })
-    );
-    global.fetch = jest.fn().mockResolvedValue({
-        json: () => Promise.resolve({ data: { token: "mock-token" } }),
-      });
-
-    const { getByText, getByPlaceholderText, getByLabelText } = render(
-      <Addtable />
-    );
-
-    // Fill in the form fields
-    fireEvent.change(getByPlaceholderText("Your Name"), {
-      target: { value: "John Doe" },
-    });
-    fireEvent.change(getByLabelText("Select Status"), {
-      target: { value: "true" },
-    });
-
-    // Submit the form
-    fireEvent.click(getByText("Add New"));
-
-    // Wait for the form submission to complete
-    await waitFor(() => {
-      expect(mockFetch).toHaveBeenCalledWith(
-        "https://mock-api.arikmpt.com/api/category/create",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer mock-token", // Replace with your mock token
-          },
-          body: JSON.stringify({
-            name: "John Doe",
-            is_active: "true",
-          }),
-        }
-      );
+describe("Addtable Component Unit Tests", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
     });
   });
-});
+  test("renders the title", () => {
+    const { getByText } = render(
+
+      <MemoryRouter>
+        <Addtable />
+      </MemoryRouter>
+    );
+    const titleElement = getByText("Add New Data");
+    expect(titleElement).toBeDefined();
+  });
+
+  test("renders the buttons", () => {
+    render(
+      <MemoryRouter>
+        <Addtable />
+      </MemoryRouter>
+    );
+    const editButtonElement = screen.getByText("Add New");
+    const backButtonElement = screen.getByText("Back");
+
+    expect(editButtonElement).toBeDefined();
+    expect(backButtonElement).toBeDefined();
+  });
+
+})
+
